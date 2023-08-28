b876aa5c8e470efedd1ecaec1173aa0f9414f213
Tedros <tedsaasfaha@gmail.com>
2023-08-28 02:56:31 -0700

    code implemented for the project 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -17,6 +17,6 @@ def safe_print_list(my_list=[], x=0):
             rtrn += 1
         except IndexError:
             break
-        print("")
-        return (rtrn)
+    print("")
+    return (rtrn)
 
