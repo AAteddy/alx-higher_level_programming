5551fda5843dbfc807ca6c3d624b7308131ca9a9
Tedros <tedsaasfaha@gmail.com>
2023-08-28 03:08:23 -0700

    0-safe_print_list.py project variable name updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,13 +10,13 @@ def safe_print_list(my_list=[], x=0):
     Returns:
         the number of elements printed.
     """
-    rtrn = 0
+    ret = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            rtrn += 1
+            ret += 1
         except IndexError:
             break
     print("")
-    return (rtrn)
+    return (ret)
 
