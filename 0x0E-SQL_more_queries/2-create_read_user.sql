9dc18e6605b33e78a8546339397ff00ce9eec6f7
Tedros <tedsaasfaha@gmail.com>
2023-10-19 01:34:53 -0700

    2-create_read_user.sql file updated by removing indentation.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,12 +2,12 @@
 -- user_0d_2 should have only SELECT privilege
 -- The user_0d_2 password should be set to user_0d_2_pwd
 CREATE DATABASE
-	IF NOT EXISTS `hbtn_0d_2`;
+IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
-	IF NOT EXISTS 'user_0d_2'@'localhost'
-	IDENTIFIED BY 'user_0d_2_pwd';
+IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-	ON `hbtn_0d_2`.*
-	TO 'user_0d_2'@'localhost'
-	IDENTIFIED BY 'user_0d_2_pwd';
+ON `hbtn_0d_2`.*
+TO 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
