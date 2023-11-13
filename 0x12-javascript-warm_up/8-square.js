b85a3035ab30408cf8bc76828a4772ca9fa7bb93
Tedros <tedsaasfaha@gmail.com>
2023-11-13 06:49:00 -0800

    8-square.js project file implemented

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+sizeSquare = Math.floor(Number(process.argv[2]));
+if (isNaN(sizeSquare)) {
+    console.log('Missing size');
+} else {
+    for (i = 1; i <= sizeSquare; i++) {
+        let strRow = '';
+        for (j = 1; j <= sizeSquare; j++) {
+            strRow += 'X'
+        }
+        console.log(strRow);
+    }
+}
