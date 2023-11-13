367230a22cc67332296ae5867958d04d5fcec596
Tedros <tedsaasfaha@gmail.com>
2023-11-13 13:07:12 -0800

    6-multi_languages_loop.js project file implemented

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-	console.log(line);
+
+let strLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (i = 0; i < strLines.length; i++) {
+	console.log(strLines[i]);
 }
