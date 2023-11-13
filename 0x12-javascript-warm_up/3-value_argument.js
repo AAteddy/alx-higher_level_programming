01b7ba23c1c503c099e06a68647e1904abba9961
Tedros <tedsaasfaha@gmail.com>
2023-11-13 05:47:57 -0800

    3-value_argument.js project file update implemented for requirement failure

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-const args = process.argv;
-if(!args[2]) 
-{
-	console.log('No argument');
-} 
-else 
-{
-	console.log(args[2]);
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
