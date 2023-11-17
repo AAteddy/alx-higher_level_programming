7b3faeba17857c7aff100f0a6324ca3b8562036e
Tedros <tedsaasfaha@gmail.com>
2023-11-17 06:02:31 -0800

    3-my_safe_filter_states.py project file implemented

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Takes in arguments and displays all values in the states table of hbtn_0e_0_usa.
+Where name matches the argument.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` \
+                ORDER BY states.id")
+    rows = c.fetchall()
+    for row in rows:
+        if(sys.argv[4] == row[1]):
+                print(row)
+    c.close()
+    db.close()
