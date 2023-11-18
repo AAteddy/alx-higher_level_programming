53f302fa122c57488db9cb9e5ca2f57f526629b8
Tedros <tedsaasfaha@gmail.com>
2023-11-18 07:39:22 -0800

    3-my_safe_filter_states.py file code updated

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 """
-Takes in arguments and displays all values in the states table
-of hbtn_0e_0_usa. Where name matches the argument.
+Hello testing
 """
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-            db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY states.id")
-    rows = c.fetchall()
-    for row in rows:
-        if(sys.argv[4] == row[1]):
-            print(row)
-    c.close()
-    db.close()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
