6968517ac5e53a4c4ac2004f0ff0023bc31ca21c
Tedros <tedsaasfaha@gmail.com>
2023-11-19 06:22:25 -0800

    14-model_city_fetch_by_state.py code updated

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """
+prints all City objects from the database hbtn_0e_14_usa.
+Results must be display as they are in the example below
+(<state name>: (<city id>) <city name>)
 """
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 from model_city import City
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
