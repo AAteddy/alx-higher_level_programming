49451ef0db4aa4d90fe8b6417510f84d65a528a0
Tedros <tedsaasfaha@gmail.com>
2023-11-18 23:29:05 -0800

    10-model_state_my_get.py project file code updated

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-prints the State object with the name passed as argument
+prints the State object with the name passed as argument.
 Results must display the states.id
 """
 
@@ -9,18 +9,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     found = False
     for state in session.query(State):
-        if(state.name == sys.argv[4]):
+        if state.name == sys.argv[4]:
             print("{}".format(state.id))
             found = True
             break
-    if(found == False):
-        print("Not Found")
+    if found is False:
+        print("Not found")
