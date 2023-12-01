a4ac807ad6c483877c96eaa3e39b374d92e9828f
Tedros <tedsaasfaha@gmail.com>
2023-12-01 05:20:42 -0800

    0-hbtn_status.py project file updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """Fetches url 'https://intranet.hbtn.io/status' using urllib module."""
-
 import urllib.request
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(req) as response:
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
