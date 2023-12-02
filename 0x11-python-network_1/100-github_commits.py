31654963686295a5da3688642f5128a1ceff02d0
Tedros <tedsaasfaha@gmail.com>
2023-12-02 11:33:23 -0800

    100-github_commits.py code updated

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,8 +9,10 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-            sys.argv[2], sys.argv[1])
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
 
     r = requests.get(url)
     commits = r.json()
