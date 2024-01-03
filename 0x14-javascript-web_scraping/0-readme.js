7b012f5a848f86d39397381255fb87a4770fdc5a
Tedros <tedsaasfaha@gmail.com>
2024-01-03 05:45:39 -0800

    0-readme.js project file updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (err, content) => {
-	if (err) throw err;
-	console.log(content);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
