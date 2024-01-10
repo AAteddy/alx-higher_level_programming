2e8bb1e214e4ad495a8760994d87af29eb8f3e44
Tedros <tedsaasfaha@gmail.com>
2024-01-10 08:59:06 -0800

    100-script.js & 101-script.js project file implemented

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+        success: function(data) {
+            $('#hello').text(data.hello);
+        }
+    });
+});
